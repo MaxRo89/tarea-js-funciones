c5beaf187df638c5226c7a78f60bf4d24d3ae528
MaxRo89 <rmax9910@gmail.com>
2026-03-16 20:48:05 -0600

    Commit Inicial

---
+++ funcion_flecha.js
@@ -0,0 +1,25 @@
+// funcion suma
+
+const sumaflecha=(a,b) => a+b
+
+console.log(sumaflecha(10,8))   
+
+
+// funcion resta
+
+const restaflecha=(a,b) => a-b
+
+console.log(restaflecha(20,5))
+
+// funcion multi
+
+const multiflecha=(a,b) => a*b
+
+console.log(multiflecha(5,7))
+
+
+// funcion division
+
+const division_flecha=(a,b) => a/b
+
+console.log(division_flecha(100,2)) 
---
+++ funcion_trad.js
@@ -0,0 +1,33 @@
+// funcion suma
+function suma(a,b){
+    return a+b
+}
+
+console.log(suma(5,8))
+
+
+// funcio resta
+function resta(a,b){
+    return a-b
+}
+
+console.log(resta(10,5))
+
+
+// funcion multiplicacion
+
+function multi(a,b){
+    return a*b
+}
+
+console.log(multi(6,5))
+
+
+// funcion division
+
+
+function division (a,b){
+    return a/b
+}
+
+console.log(division(10,5))
